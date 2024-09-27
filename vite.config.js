53789087e7fab81e81cfdef5188c2055b16554b9
olesyamaniuk <maniukolesya222@gmail.com>
2024-09-27 19:18:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -31,11 +31,46 @@
 //   };
 // });
 
+// import { defineConfig } from 'vite';
+// import glob from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+// import react from '@vitejs/plugin-react-swc';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+//       rollupOptions: {
+//         input: glob.sync('/src/index.html'), // Ensure this path matches your HTML files
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: 'commonHelpers.js',
+//         },
+//       },
+//       outDir: '../dist',
+//     },
+//     plugins: [react(), injectHTML(), FullReload(['./src/**/**.html'])],
+//   };
+// });
+
+
+
+
+
 import { defineConfig } from 'vite';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig(({ command }) => {
   return {
@@ -46,7 +81,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('/src/index.html'), // Ensure this path matches your HTML files
+        input: path.resolve(__dirname, './src/index.html'), // Directly pointing to the HTML file
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -56,8 +91,11 @@ export default defineConfig(({ command }) => {
           entryFileNames: 'commonHelpers.js',
         },
       },
-      outDir: '../dist',
+      outDir: 'dist',
     },
     plugins: [react(), injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
+
+
+
